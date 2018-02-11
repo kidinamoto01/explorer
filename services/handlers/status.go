0d3667e9a9f47d25d087b8c4f9eb9c5a7de537b9
kidinamoto01 <kidinamoto@qq.com>
2018-02-11 14:43:47 +0800

    add height

--- services/handlers/status.go
+++ services/handlers/status.go
@@ -18,9 +18,27 @@ func queryStatus(w http.ResponseWriter, r *http.Request) {
     return
   }
   if err := printResult(w, status); err != nil {
-    common.WriteError(w, err)    
+    common.WriteError(w, err)
   }
 }
+
+
+//query the block height
+// queryBlock is to query a block by height
+func queryBlockHeight(w http.ResponseWriter, r *http.Request) {
+
+  c := commands.GetNode()
+  status, err := c.Status()
+
+  if err != nil {
+    common.WriteError(w, err)
+    return
+  }
+  if err := printResult(w, status.LatestBlockHeight); err != nil {
+    common.WriteError(w, err)
+  }
+}
+
 
 // mux.Router registrars
 
@@ -29,9 +47,16 @@ func RegisterQueryStatus(r *mux.Router) error {
   return nil
 }
 
+//register
+func RegisterQueryBlockHeight(r *mux.Router) error {
+  r.HandleFunc("/status/height", queryBlockHeight).Methods("GET")
+  return nil
+}
+
 func RegisterStatus(r *mux.Router) error {
   funcs := []func(*mux.Router) error{
     RegisterQueryStatus,
+    RegisterQueryBlockHeight,
   }
 
   for _, fn := range funcs {
